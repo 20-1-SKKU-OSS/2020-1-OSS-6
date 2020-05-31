1b6de3e5961ffce85685f5c053792efab87a0850
Qayin <v37794@g.skku.edu>
2020-05-31 18:41:22 +0900

    bubbleDemo한글화

--- algorithms_and_data_structures/sort_search_problems/bubbleSortDemo.cpp
+++ algorithms_and_data_structures/sort_search_problems/bubbleSortDemo.cpp
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+//bubble sort는 배우 비효율 적이니 배열 내 원소의 수가 매우 적을 때만 사용하시오.
 int main()
 {
     const int MAX_ELEMENTS = 10;
@@ -30,24 +31,24 @@ int main()
     double arrD[MAX_ELEMENTS];
 	srand(time(0));
 	
-    //Filling up the array with random numbers;
+    //배열에 랜덤 숫자들로 채워 넣는다.
     for (int i = 0; i < MAX_ELEMENTS; ++i)
     {   
-        // arr[i] = algo::random_range(1, 100);	//does not generate random number everytime
+        // arr[i] = algo::random_range(1, 100);	//does not generate random number everytime  
         //arrD[i] = algo::random_range(1.0, 99.99);	//does not generate random
-		arr[i] = rand() % 100 + 1;
-		double f = (double)rand() / RAND_MAX;
-		arrD[i] = 1.0 + f * (99.99 - 1.0);
+		arr[i] = rand() % 100 + 1;		//1부터 100까지의 수로 arr을 채워 넣는다.
+		double f = (double)rand() / RAND_MAX;	
+		arrD[i] = 1.0 + f * (99.99 - 1.0);	// 1.0부터 99.99까지의 수로 랜덤수를 arrD에 채워 넣는다. 
     }
 
-    std::cout << "Before Sorting:\n";
+    std::cout << "Before Sorting:\n";	//정렬하기 전의 배열의 상태를 보여준다.
     algo::printList(arr, MAX_ELEMENTS);
-    algo::bubbleSort(arr, 0, MAX_ELEMENTS - 1);
+    algo::bubbleSort(arr, 0, MAX_ELEMENTS - 1);	//정렬 후의 배열 상태를 보여준다.
     std::cout << "After Sorting:\n";
     algo::printList(arr, MAX_ELEMENTS);
 
 
-    std::cout << "\n\nBefore Sorting:\n";
+    std::cout << "\n\nBefore Sorting:\n";	// 실수 범위에 대해서 위과 같은 과정을 반복한다.
     algo::printList(arrD, MAX_ELEMENTS);
     algo::bubbleSort(arrD, 0, MAX_ELEMENTS - 1);
     std::cout << "After Sorting:\n";
