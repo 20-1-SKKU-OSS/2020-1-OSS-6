d0260bb40b07f7285ab31ea806626b66671f4a86
Qayin <v37794@g.skku.edu>
2020-06-01 23:07:06 +0900

    SleepSort.cpp

--- algorithms_and_data_structures/sort_search_problems/SleepSort.cpp
+++ algorithms_and_data_structures/sort_search_problems/SleepSort.cpp
@@ -1,4 +1,4 @@
-//This is the most simple implementation of sleeping sort in c++//
+//이것은 c++에서 잠자기 정렬 중 가장 간단한 방법입니다.
 #include<iostream>
 #include<thread>
 #include<vector>
@@ -10,25 +10,25 @@ void sleep(int);
 
 int main()
 {
-	//initialize an array of integers to be sorted
-	// Doesn't work for negative numbers
+	//정렬을 하기 위해서 정수 배열을 초기화합니다.
+	//음수에 대해서는 반응하지 마세요.
 	int arr[] = { 3,2,1 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	
-	//find the max element in arr
-	//Why to ding max? Read below
+
+	//배열에서 가장 큰 값을 찾아
+	//왜 최대값을 찾는지는 아래를 보세요.	
 	int max = arr[0];
 	for (int i = 0; i < n; i++) {
 		if (arr[i] > max) max = arr[i];
 	}
 	max++;
 
-	//call the function SLEEPSORT()
+	//SLEEPSORT()라는 함수를 호출합니다.
 	sleepSort(arr, n);
 
-	//make the main thread to go to sleep for 1 second more then
-	//the largest number in the list.
-	//this ensures all the threads are over before main finishes execution.
+	//주요 스레드를 1초이상 잠자도록 합니다.
+	//리스트에서 가장 큰 수를 
+	//이것은 메인 함수가 끝나기 전에 모든 쓰레드가 끝나도록 보장합니다.
 	std::this_thread::sleep_for(std::chrono::seconds(max));
 
 
@@ -36,7 +36,7 @@ int main()
 }
 
 void sleep( int i) {
-	//make the thread to sleep for I seconds.
+	//I초 종안 스레드가 잠자도록 합니다
 	std::this_thread::sleep_for(std::chrono::seconds(i));
 	cout << i << " ";
 }
@@ -45,7 +45,7 @@ void sleepSort(int* arr, int n) {
 	thread* threads;
 	threads = new thread[n];
 	for (int i = 0; i < n; i++) {
-		//a new thread is created for each element of an array, which goes to sleep for arr[i] seconds.
+		//새로은 쓰레드는 배열의 각각의 원소들을 위해서  만들어집니다. 그 배열은 arr[i] 초 동안 잠에 듭니다.
 		threads[i] = (thread(sleep, arr[i]));
 	}
 }
