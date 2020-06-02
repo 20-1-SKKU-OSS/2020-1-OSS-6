d6c950ad71d85dd1ea21d7b6f5b977fec18963e6
Qayin <v37794@g.skku.edu>
2020-06-02 23:56:32 +0900

    missingNumber2.cpp

--- algorithms_and_data_structures/sort_search_problems/missingNumber2.cpp
+++ algorithms_and_data_structures/sort_search_problems/missingNumber2.cpp
@@ -1,11 +1,12 @@
 /**
- * Find the missing number in Arithmetic Progression
- * Examples:
- * Input: arr[]  = {2, 4, 8, 10, 12, 14}
- * Output: 6
  *
- * Input: arr[]  = {1, 6, 11, 16, 21, 31};
- * Output: 26
+ * 다음에 나열된 수가 수학적으로 의미있기 위해서 추가해야 하는 수를 찾으시오.
+ * 예시)
+ * 입력: arr[]  = {2, 4, 8, 10, 12, 14}
+ * 출력: 6
+ *
+ * 입력: arr[]  = {1, 6, 11, 16, 21, 31};
+ * 출력: 26
  */
 
 #include <iostream>
@@ -17,18 +18,15 @@ int findMissingUtil( std::vector<int> & vec, int diff) {
 	size_t high = vec.size() - 1;
 	while ( low < high ) {
 		size_t mid = ( low + high ) / 2;
-
-		//check if the missing element just after mid index is missing.
+		//중간에 있는 값 바로 답음에 빠진 원소가 우리가 찾는 추가해야 하는 원소인지 확인하시오.
 		if ( mid < vec.size() - 1 && vec[mid + 1] - vec[mid] != diff ) {
 			return vec[mid] + diff;
 		}
-
-		// the element just before mid is missing.
+		// 중간 전의 원소가 추가해야 하는 값이면
 		if ( mid > 0 && vec[mid-1] - vec[mid] != diff ) {
 			return vec[mid - 1]  + diff;
 		}
-
-		//the sub array before mid is following AP
+		//중간 이전의 하위 배열이 AP를 따른다.
 		if ( vec[mid] == vec[0] + diff * mid ) {
 			low = mid + 1;
 		} else {
@@ -54,7 +52,7 @@ int missingNumber( std::vector<int> & vec ) {
 int main() {
 	std::vector<int> vec{ 2, 4, 8, 10, 12, 14 };
 	printVec(vec);
-	std::cout << "Missing number in above arithmetic progression is : "
+	std::cout << "위의 수학적 나열 중에서 빠진 수는 : "
 			  << missingNumber(vec) << std::endl;
 	return 0;
 }
