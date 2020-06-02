a739d68ed632a1e8865652dca14ebe5b2bfe960f
Qayin <v37794@g.skku.edu>
2020-06-03 00:05:22 +0900

    missingNumbers.cpp

--- algorithms_and_data_structures/sort_search_problems/missingNumbers.cpp
+++ algorithms_and_data_structures/sort_search_problems/missingNumbers.cpp
@@ -1,29 +1,30 @@
 /**
+ * 문제 설명
  * Problem Statement
  *
- * Numeros, the Artist, had two lists A and B, such that B was a permutation of A.
- * Numeros was very proud of these lists. Unfortunately, while transporting them from one exhibition to another, some numbers were left out of A.
- * Can you find the missing numbers?
+ * 그 아티스트인 민수씨는'가'와 '나'라는  두 리스트를 가지고 있다. '나'는 '가'의 순열이다.
+ * 민수씨는 그 리스트에 대해서 자부심을 가지고 있었다. 불행히도 전시장에서 다른 전시장으로 옮기던 중 몇 가지 수가 '가'에서 빠졌다.
+ * 당신은 그 수를 찾을 수 있겠는가?
+ * 
+ * 주의사항
  *
- * Notes
+ * 만약 그 리스트 안에서 어떤 한 수가 여러 개 있다면, 당신은 반드시 양쪽의 리스트에 두 수가 같은 횟수 만큼 등장한다는 사실을 알아야 한다.
+ * 만약 그 경우가 아니라면 그 수는 반드시 빠진 수다.
+ * 당신은 오름차순으로 빠진 수들을 출력해야 한다.
+ * '나'리스트에서 가장 큰 수와 가장 작은 수의 차이는 100이하다.
+ * 
+ * 입력 형식
+ * 네 줄이 입력된다.
  *
- * If a number occurs multiple times in the lists, you must ensure that the frequency of that number in both lists is the same.
- * If that is not the case, then it is also a missing number.
- * You have to print all the missing numbers in ascending order.
- * Print each missing number once, even if it is missing multiple times.
- * The difference between maximum and minimum number in B is less than or equal to 100.
- * Input Format
- * There will be four lines of input:
+ * n- 첫 번째 리스트의 크기
+ * 첫 번째 리스트를 이루는 n 개의 정수 배열이 입력된다.
+ * m- 두 번째 리스트의 크기
+ * 두 번째 리스트를 이루는 m 개의 정수 배열이 입력된다.
  *
- * n - the size of the first list
- * This is followed by n space-separated integers that make up the first list.
- * m - the size of the second list
- * This is followed by m space-separated integers that make up the second list.
+ * 출력 형식
+ * 오름차순으로 빠진 숫자를 출력해라.
  *
- * Output Format
- * Output the missing numbers in ascending order.
  */
-
 #include <cmath>
 #include <cstdio>
 #include <vector>
