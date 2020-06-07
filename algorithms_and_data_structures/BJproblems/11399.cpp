a7cea8209c982d1d98ca53de69eb90b77d590ddb
andy9705 <63641316+andy9705@users.noreply.github.com>
2020-06-08 04:04:19 +0900

    Update 11399.cpp
    코드 scanf printf 부분 수정해주었습니다.

--- algorithms_and_data_structures/BJproblems/11399.cpp
+++ algorithms_and_data_structures/BJproblems/11399.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int main(){
 int arr[10005], N, result = 0;
-scanf("%d",&N);
+cin >> N;
 for(int i = 0 ; i <N;i++)
-    scanf("%d",&arr[i]);
+    cin >> arr[i];
 sort(arr,arr+N);
 for(int i = 0; i < N; i++)
     result += arr[i]*(N-i);
-printf("%d",result);
+cout << result;
 }
 
 
