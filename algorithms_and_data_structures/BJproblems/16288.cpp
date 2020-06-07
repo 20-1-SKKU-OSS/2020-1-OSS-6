788cd14763bdc9668ff2d77a5151af15d8c0d61a
Qayin <v37794@g.skku.edu>
2020-06-07 23:59:44 +0900

    16288

---
+++ algorithms_and_data_structures/BJproblems/16288.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+int N,K;
+cin >> N >> K;
+int InputArr[N], RollBackArr[K];
+for (int i = 0; i <K ; i++)
+    RollBackArr[i] = N+1;
+for(int i = 0; i < N; i++)
+    cin >> InputArr[i];
+
+
+bool BigSuccess = 1;
+for (int i = N-1; i >=0; i --){
+    int MinDefference = N+2, MinPosition = K;
+    for(int j = 0; j < K ; j ++){
+        if (InputArr[i] < RollBackArr[j] && RollBackArr[j] - InputArr[i] < MinDefference){
+            MinPosition = j;
+            MinDefference = RollBackArr[j] - InputArr[i];
+        }
+
+    }
+
+    if (MinPosition == K){
+        BigSuccess = 0;
+        break;
+    }
+    RollBackArr[MinPosition] = InputArr[i];
+}
+
+if (BigSuccess == 0)
+    cout << "NO";
+else
+    cout << "YES";
+
+}
+
+
